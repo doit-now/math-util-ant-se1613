20d99a3b4ab51280b63b5016f735b9765ef2d504
doit-now <hoang.nguyenthe@gmail.com>
2022-10-15 09:04:43 +0700

    update getfactorial() to use recursion

--- src/com/giaolang/mathutil/core/MathUtil.java
+++ src/com/giaolang/mathutil/core/MathUtil.java
@@ -22,23 +22,45 @@ public class MathUtil {
     //Vì giai thừa tăng cực nhanh, nên 21! đã vượt 18 số 0
     //tràn kiểu long.
     //Ta ko tính 21! trở lên
+    
+    //SỬA HÀM TÍNH GIAI THỪA = CÁCH XÀI ĐỆ QUY!!!
+    //n! = 1.2.3.4.5...n
+    //5! = 1.2.3.4.5
+    //5! = 4! x 5 = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1!
+    //1! = 1 chốt dừng dội ngược lên
+    //n! = n x (n - 1)! công thức đệ quy
+    //N! = RETURN N X (N - 1)!
+    //ĐỆ QUY: GỌI LẠI CHÍNH MÌNH VỚI 1 QUY MÔ KHÁC/NHỎ HƠN
+    //CON BÚP BÊ NGA
     public static long getFactorial(int n) {
 
         if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
       
-        if (n == 0 || n == 1) 
-            return 1;
-        
-        //sống sót đến đây, sure n = 2...20!!!
-        //CẤM KO XÀI ELSE NỮA KHI HÀM ĐÃ CÓ RETURN PHÍA TRƯỚC
-        long product = 1; //biến cộng dồn, nhân dồn, biến con heo đất
-        //acc/accumulation/gửi góp
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-        return product;
+        return n * getFactorial(n - 1);
+       
     }
+    
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20");
+//      
+//        if (n == 0 || n == 1) 
+//            return 1;
+//        
+//        //sống sót đến đây, sure n = 2...20!!!
+//        //CẤM KO XÀI ELSE NỮA KHI HÀM ĐÃ CÓ RETURN PHÍA TRƯỚC
+//        long product = 1; //biến cộng dồn, nhân dồn, biến con heo đất
+//        //acc/accumulation/gửi góp
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//    }
 }
 //CODING CONVENTION: QUY TẮC VIẾT CODE CTY ÉP PHẢI THEO!!!
 //ALT-SHIFT-F CĂN CODE TỰ ĐỘNG, KO LẠM DỤNG
